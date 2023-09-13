72ee0858bb5ef254fd5036b1aff7f8a3cd9e6b74
Ananda Rizki <rizki.id@gmail.com>
2023-09-14 06:32:09 +0700

    Remove default export in .d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,4 @@ export function jaccardSimilarity(str1: string, str2: string): number;
 
 export function ngramSimilarity(str1: string, str2: string, n?: number): number;
 
-export default function compareText(str1: string, str2: string): number;
+export function compareText(str1: string, str2: string): number;
